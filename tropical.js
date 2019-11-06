717fae9a9ec33adfba9a5daf0fdb176096dc800e
jchack1 <55161597+jchack1@users.noreply.github.com>
2019-11-05 18:32:38 -0700

    updated tropicals, text works, js works

---
+++ tropical.js
@@ -0,0 +1,54 @@
+const hana = document.getElementById("tropical-hana");
+const snake = document.getElementById("tropical-snake");
+const aloe1 = document.getElementById("tropical-aloe-1");
+const deacon = document.getElementById("tropical-deacon");
+const bambam = document.getElementById("tropical-bambam");
+const betina = document.getElementById("tropical-betina");
+const ellie = document.getElementById("tropical-ellie");
+const viago = document.getElementById("tropical-viago");
+const aloe2 = document.getElementById("tropical-aloe-2");
+
+hana.addEventListener("click", function(){
+    document.getElementById("tropical-hana-img").classList.toggle("visible");
+    document.getElementById("tropical-hana-text").classList.toggle("invisible");
+})
+
+snake.addEventListener("click", function(){
+    document.getElementById("tropical-snake-img").classList.toggle("visible");
+    document.getElementById("tropical-snake-text").classList.toggle("invisible");
+})
+
+aloe1.addEventListener("click", function(){
+    document.getElementById("tropical-aloe-1-img").classList.toggle("visible");
+    document.getElementById("tropical-aloe-1-text").classList.toggle("invisible");
+})
+
+deacon.addEventListener("click", function(){
+    document.getElementById("tropical-deacon-img").classList.toggle("visible");
+    document.getElementById("tropical-deacon-text").classList.toggle("invisible");
+})
+
+bambam.addEventListener("click", function(){
+    document.getElementById("tropical-bambam-img").classList.toggle("visible");
+    document.getElementById("tropical-bambam-text").classList.toggle("invisible");
+})
+
+betina.addEventListener("click", function(){
+    document.getElementById("tropical-betina-img").classList.toggle("visible");
+    document.getElementById("tropical-betina-text").classList.toggle("invisible");
+})
+
+ellie.addEventListener("click", function(){
+    document.getElementById("tropical-ellie-img").classList.toggle("visible");
+    document.getElementById("tropical-ellie-text").classList.toggle("invisible");
+})
+
+viago.addEventListener("click", function(){
+    document.getElementById("tropical-viago-img").classList.toggle("visible");
+    document.getElementById("tropical-viago-text").classList.toggle("invisible");
+})
+
+aloe2.addEventListener("click", function(){
+    document.getElementById("tropical-aloe-2-img").classList.toggle("visible");
+    document.getElementById("tropical-aloe-2-text").classList.toggle("invisible");
+})
